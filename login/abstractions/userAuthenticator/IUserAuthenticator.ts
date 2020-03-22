8202288ad95486048a5a04dc6feef226d7f5277d
gurk7 <keinangur@gmail.com>
2020-03-22 21:33:17 +0200

    updated comment in userAuthenticator

--- login/abstractions/userAuthenticator/IUserAuthenticator.ts
+++ login/abstractions/userAuthenticator/IUserAuthenticator.ts
@@ -1,7 +1,7 @@
 import { User } from "../../../entities/user";
 
-//this class is Generic because RetrieveUser can return User | null directly in  a Synchronous code
-// or a Promise<User | null> in an Asynchronous code
+//this class is Generic because RetrieveUser can return boolean in  a Synchronous code
+// or a Promise<boolean> in an Asynchronous code
 
 export interface IUserAuthenticator<T> {
   authenticate(inputUser: User): T;
